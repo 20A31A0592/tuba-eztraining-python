bc3dcfa26abbc9bc304df2463e7a241a647bfe0b
20A31A0592 <109903075+20A31A0592@users.noreply.github.com>
2023-02-02 09:40:36 +0530

    Add files via upload

---
+++ DAY_1_PROGRAMS.py
@@ -0,0 +1,87 @@
+'''swap without temp variable
+n1,n2=int(input("Enter:")),int(input("Enter:"))
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print(n1,n2)'''
+
+'''Another Method
+n1,n2=int(input("Enter:")),int(input("Enter:"))
+n1=n1*n2
+n2=n1//n2
+n1=n1//n2
+print(n1,n2)'''
+
+'''Another Method
+n1,n2=int(input("Enter:")),int(input("Enter:"))
+n1=n1**n2
+n2=n1**n2
+n1=n1**n2
+print(n1,n2)'''
+
+'''Area of Rectangle'''
+l,b=int(input("Enter:")),int(input("Enter:"))
+area=l*b
+print(area)
+
+'''Perimeter of Rectangle'''
+l,b=int(input("Enter:")),int(input("Enter:"))
+peri=(2*(l+b))
+print(peri)
+
+print(chr(3128)+chr(3108)+chr(3183+3119))
+
+Totalsugar= 75
+samsugar=Totalsugar/2
+kumarsugar=samsugar/2
+totalkumarsugar=samsugar+kumarsugar
+print(totalkumarsugar)
+print(kumarsugar)
+print(samsugar)
+
+num1=int(input("enter positive num:"))
+num2=float(input("enter negative floating value"))
+mul=num1*num2
+print(mul)
+
+num=36.32
+mul=3*num
+total=56.19+mul
+sub=total-10
+finalresult=sub
+print("finalresult:",finalresult)
+
+a,b=input("enter").split(",")
+print(a)
+print(type(a))
+print(b)
+a+b
+
+FirstInteger=int(input("enter first integer :"))
+SecondInteger=int(input("enter second integer :"))
+ThirdInteger=int(input("enter third integer :"))
+
+
+FirstFloat=float(input("enter first float :"))
+SecondFloat=float(input("enter second float :"))
+ThirdFloat=float(input("enter third float :"))
+
+Firststring=(input("enter first string :"))
+Secondstring=(input("enter second string :"))
+
+Firstcomplex=(input("enter complex number:"))
+
+
+print(FirstInteger)
+print(SecondInteger)
+print(ThirdInteger)
+
+print(FirstFloat)
+print(SecondFloat)
+print(ThirdFloat)
+
+print(Firststring)
+print(Secondstring)
+
+
+print(Firstcomplex)
